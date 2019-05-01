735381bb86c7ce1c443f8faf9d4534d28162d387
Ozan Cansel <ozancansel@gmail.com>
2019-05-01 10:51:44 +0300

    timer removed from auto_serialize example

--- src/examples/qobject/auto_serialize.cpp
+++ src/examples/qobject/auto_serialize.cpp
@@ -1,5 +1,4 @@
 #include <QCoreApplication>
-#include <QTimer>
 #include <QThread>
 #include <speech/udp/udp_transmitter.h>
 #include <speech/qobject_serialization.h>
@@ -8,27 +7,25 @@
 // template QDataStream& operator<<(QDataStream& , const entity&);
 // template QDataStream& operator>>(QDataStream& , entity&);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     using namespace speech::udp;
     using namespace speech;
 
-    QCoreApplication app(argc , argv);
-    QCoreApplication::setApplicationName("speech-udp-transmitter");
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("speech-udp-qobject-serialization-transmitter");
 
-    QTimer::singleShot(10 , [](){
-        udp_transmitter<entity> transmitter { QHostAddress { QHostAddress::LocalHost } , speech::port(12345) };
-        for(int i = 0; ; ++i)
-        {
-            entity e;
-            e.set_name(QString("Ozan - %0").arg(i));
-            e.set_identity(i * rand() % 100);
-            transmitter.transmit(e);
-            QCoreApplication::processEvents();
+    udp_transmitter<entity> transmitter{QHostAddress{QHostAddress::LocalHost}, speech::port(12345)};
+    for (int i = 0;; ++i)
+    {
+        entity e;
+        e.set_name(QString("Ozan - %0").arg(i));
+        e.set_identity(i * rand() % 100);
+        transmitter.transmit(e);
+        QCoreApplication::processEvents();
 
-            QThread::msleep(1000);
-        }
-    });
+        QThread::msleep(1000);
+    }
 
     return app.exec();
 }
