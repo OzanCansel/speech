1655bcf55b76eadc4bf5bf965985a98ba91fedec
Ozan Cansel <ozancansel@gmail.com>
2019-05-01 10:46:47 +0300

    timer removed from transmit example

--- src/examples/udp/transmit.cpp
+++ src/examples/udp/transmit.cpp
@@ -8,30 +8,28 @@
 #include <speech/udp/udp_transmitter.h>
 #include "models.h"
 
-int main(int argc , char** argv)
+int main(int argc, char **argv)
 {
     using namespace speech::udp;
 
-    QCoreApplication app(argc , argv);
+    QCoreApplication app(argc, argv);
     QCoreApplication::setApplicationName("speech-udp-transmitter");
 
-    QTimer::singleShot(10 , [](){
-        udp_transmitter<greeting , roll_dice> transmitter { QHostAddress { QHostAddress::LocalHost } , speech::port(12345) };
-        for(int i = 0; ; ++i)
-        {
-            greeting greeting_msg;
-            greeting_msg.my_name_is = QString("%0 - %1").arg("Hi ! My name is Ozan -").arg(i);
-            transmitter.transmit(greeting_msg);
-            qDebug() << "transmit => " << greeting_msg;
-            QCoreApplication::processEvents();
-            // QThread::msleep(rand() % 10);
-            roll_dice dice;
-            transmitter.transmit(dice);
-            qDebug() << "transmit => " << dice;
-            QCoreApplication::processEvents();
-            // QThread::msleep(rand() % 10);
-        }
-    });
+    udp_transmitter<greeting, roll_dice> transmitter{QHostAddress{QHostAddress::LocalHost}, speech::port(12345)};
+    for (int i = 0;; ++i)
+    {
+        greeting greeting_msg;
+        greeting_msg.my_name_is = QString("%0 - %1").arg("Hi ! My name is Ozan -").arg(i);
+        transmitter.transmit(greeting_msg);
+        qDebug() << "transmit => " << greeting_msg;
+        QCoreApplication::processEvents();
+        QThread::msleep(rand() % 1000);
+        roll_dice dice;
+        transmitter.transmit(dice);
+        qDebug() << "transmit => " << dice;
+        QCoreApplication::processEvents();
+        QThread::msleep(rand() % 1000);
+    }
 
     return app.exec();
 }
