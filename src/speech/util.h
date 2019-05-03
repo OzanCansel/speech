c0d2c9f82d6d1662592e6770db51dbdfc15a2a67
Ozan Cansel <ozancansel@gmail.com>
2019-05-03 17:46:24 +0300

    type identifying mechanism is implemented and unified for gcc, cland and msvc compilers

--- src/speech/util.h
+++ src/speech/util.h
@@ -4,12 +4,20 @@
 #include <QString>
 #include <type_traits>
 #include <typeinfo>
+#include <regex>
 
 namespace speech
 {
 
 namespace impl
 {
+
+    #define GCC_COMPILER (defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER))
+    #define CLANG_COMPILER (defined(__clang__))
+
+    #if GCC_COMPILER || CLANG_COMPILER
+        #include <cxxabi.h>
+    #endif
 
 template <typename S, typename T>
 class implements_left_stream
@@ -42,7 +50,24 @@ class implements_right_stream
 
 template<class T>
 QString identify()
-{
+{   
+
+    #if defined(_MSC_BUILD)
+
+        std::regex pat{ R"(^.+\s)" };    
+        auto unified_str = std::regex_replace(typeid(T).name() , pat , "");
+        return QString::fromStdString(unified);
+
+    #endif
+
+    #if GCC_COMPILER || CLANG_COMPILER
+        int status;
+        auto class_name = abi::__cxa_demangle(typeid(T).name() , 0 , 0 , &status);
+
+        return QString::fromLatin1(class_name);
+    #endif
+
+    //Default identifier
     return  QString::fromStdString(typeid(T).name());
 }
 
@@ -50,12 +75,12 @@ template <typename T, typename param>
 class named_type
 {
 public:
-    explicit named_type(T const& value) : value_(value) {}
-    explicit named_type(T&& value = T{}) : value_(std::move(value)) {}
-    T& get() { return value_; }
-    T const& get() const {return value_; }
+    explicit named_type(T const& value) : m_value(value) {}
+    explicit named_type(T&& value = T{}) : m_value(std::move(value)) {}
+    T& get() { return m_value; }
+    T const& get() const {return m_value; }
 private:
-    T value_;
+    T m_value;
 };
 
 } // namespace impl
