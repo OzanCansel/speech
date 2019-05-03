d2ca80123e7cbe09e56348ab6b68f0a449ab0393
Ozan Cansel <ozancansel@gmail.com>
2019-05-03 17:34:14 +0300

    auto_serialize arranged as simple cli.

--- src/examples/qobject/auto_serialize.cpp
+++ src/examples/qobject/auto_serialize.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
 #include <QThread>
 #include <speech/udp/udp_transmitter.h>
 #include <speech/qobject_serialization.h>
@@ -11,24 +12,58 @@ int main(int argc, char **argv)
     using namespace speech;
 
     QCoreApplication app(argc, argv);
-    QCoreApplication::setApplicationName("speech-udp-qobject-serialization-transmitter");
 
-    udp_transmitter<entity , car_info> transmitter{QHostAddress{QHostAddress::LocalHost}, speech::port(12345)};
+    QCommandLineParser parser;
+    parser.addHelpOption();
+
+    parser.addOptions(
+        {{ {"p", "port"} , "Specify port number" , "port number" },
+         { {"f", "fps"} , "Fps indicates the transmitting speed" , "fps"}
+         });
+
+    parser.process(app);
+
+    //Defaults
+    auto port = 12345;
+    auto fps = 2;
+
+    if (parser.isSet("p"))
+    {
+        qDebug() << parser.value("p");
+        port = parser.value("p").toInt();
+    }
+    if (parser.isSet("f"))
+        fps = parser.value("f").toInt();
+
+    //twice in a second
+    auto delay = 500.0 / fps;
+
+    udp_transmitter<entity, car_info> transmitter{QHostAddress{QHostAddress::LocalHost}, speech::port(port)};
+
+    qDebug() << "Starting to transmit on port " << port << " with " << fps << " fps";
 
     for (int i = 0;; ++i)
     {
         entity e;
-        e.set_name(QString("Ozan - %0").arg(i));
+        e.set_name(QString("ozan - %0").arg(i));
         e.set_identity(i * rand() % 100);
         transmitter.transmit(e);
+
+        qDebug() << "transmit => " << e;
+
+        QCoreApplication::processEvents();
+
+        QThread::msleep(delay);
 
         car_info info;
         info.set_name(QString("chevrolet - %0").arg(i));
         transmitter.transmit(info);
 
+        qDebug() << "transmit => " << info;
+
         QCoreApplication::processEvents();
 
-        QThread::msleep(1000);
+        QThread::msleep(delay);
     }
 
     return app.exec();
