39f928eb9a0d3d65366c4fa8fe8ea26c1a3e2be8
Ozan Cansel <ozancansel@gmail.com>
2019-05-03 17:47:05 +0300

    qobject serialization algorithm changed to string based property assignment

--- src/speech/qobject_serialization.h
+++ src/speech/qobject_serialization.h
@@ -6,6 +6,7 @@
 #include <QDebug>
 #include <QMetaObject>
 #include <QMetaProperty>
+#include <vector>
 #include <type_traits>
 
 namespace speech
@@ -15,6 +16,8 @@ namespace speech
     {
         auto meta_info = obj.metaObject();
 
+        std::vector<int> writable_property_indexes;
+
         for(auto i = 0; i < meta_info->propertyCount(); ++i)
         {
             auto prop = meta_info->property(i);
@@ -22,8 +25,16 @@ namespace speech
             if(prop.isConstant() || !prop.isWritable() || !prop.isReadable())
                 continue;
 
-            qDebug() << prop.name() << " <= " << prop.read(&obj);
+            writable_property_indexes.push_back(i);
+        }
 
+        out << static_cast<int>(writable_property_indexes.size());
+
+        for(auto index : writable_property_indexes)
+        {
+            auto prop = meta_info->property(index);
+
+            out << QString::fromLatin1(prop.name());
             out << prop.read(&obj);
         }
 
@@ -35,19 +46,30 @@ namespace speech
     {
         auto meta_info = obj.metaObject();
 
-        for(auto i = 0; i < meta_info->propertyCount(); ++i)
+        int count_of_properties{ };
+        in >> count_of_properties;
+
+        for(auto i = 0; i < count_of_properties; ++i)
         {
-            auto prop = meta_info->property(i);
+            
+            //Read property name and value
+            QString prop_name;
+            QVariant prop_val;
+
+            in >> prop_name;
+            in >> prop_val;
+
+            auto prop_idx = meta_info->indexOfProperty(prop_name.toStdString().c_str());
+
+            if(prop_idx < 0)
+                continue;
+
+            auto prop = meta_info->property(prop_idx);
 
             if(prop.isConstant() || !prop.isWritable() || !prop.isReadable())
                 continue;
 
-            QVariant variant;
-            in >> variant;
-
-            qDebug() << prop.name() << " => " << variant;
-
-            prop.write(&obj , variant);
+            prop.write(&obj , prop_val);
         }
 
         return in;
@@ -56,6 +78,22 @@ namespace speech
     template<class T>
     typename std::enable_if<std::is_base_of<QObject , T>::value , QDebug>::type operator<<(QDebug out , const T& obj)
     {
+
+        auto meta_info = obj.metaObject();
+
+        for(auto i = 0; i < meta_info->propertyCount(); ++i)
+        {
+            auto prop = meta_info->property(i);
+
+            if(!prop.isReadable())
+                continue;
+
+            auto prop_name = prop.name();
+            auto prop_value = prop.read(&obj);
+
+            out << prop_name << " : " << prop_value << " , ";
+        }
+
         return out;
     }
 }
