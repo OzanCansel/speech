71b5e014bcfcf34ee9951a00719c1c3e0bfea4ac
Ozan Cansel <ozancansel@gmail.com>
2019-06-14 21:02:49 +0300

    shared socket made suitable for both QUdpSocket and QTcpSocket

--- src/speech/shared_socket.h
+++ src/speech/shared_socket.h
@@ -2,18 +2,41 @@
 #define SPEECH_SHARED_SOCKET_H
 
 #include <QTcpSocket>
+#include <QUdpSocket>
+#include <functional>
+#include "speech/handle/handle.h"
+#include "speech/handle/shared_ptr_handle.h"
+#include "speech/handle/unique_ptr_handle.h"
 
 namespace speech
 {
-        template<typename T>
-        class shared_socket
-        {
-            // static_assert(false , "T socket is not implemented yet.");
-        };
 
-        template<>
-        class shared_socket<QTcpSocket>;
+namespace impl
+{
+    QByteArray read_all(QTcpSocket&);
+    QByteArray read_all(QUdpSocket&);
 }
+
+template <typename Socket>
+class shared_socket
+{
+public:
+    shared_socket(Socket &);
+    shared_socket(std::shared_ptr<Socket>);
+    shared_socket(std::unique_ptr<Socket>);
+
+    inline Socket &socket();
+    void attach(std::function<int(const QByteArray &)>);
+    void listen();
+    void on_data_received();
+
+private:
+    std::unique_ptr<speech::handle::handle<Socket>> m_socket;
+    QByteArray m_buffer;
+    std::vector<std::function<int(const QByteArray &)>> m_listeners;
+};
+
+} // namespace speech
 
 #include "shared_socket_impl.h"
 
