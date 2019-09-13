fe824e147345c96b0f8a07694b691dce807156a0
Ozan Cansel <ozan.cansel@armelsan.local>
2019-09-13 13:41:31 +0300

    is_container type_trait checks begin(), end(), size(), push_back(int), at(int) functions from now on.

--- src/speech/traits/is_container.h
+++ src/speech/traits/is_container.h
@@ -7,11 +7,85 @@
 namespace speech
 {
 
-    template <typename Container>
-    struct is_container : std::false_type { };
+    template <typename S>
+    class has_begin_and_end
+    {
 
-    template <typename... Ts> struct is_container<std::list<Ts...> > : std::true_type { };
-    template <typename... Ts> struct is_container<std::vector<Ts...> > : std::true_type { };
+        template <typename SS>
+        static auto test_begin(int)
+            -> decltype(std::declval<SS>().begin() , std::true_type());
+
+        template <typename>
+        static auto test_begin(...) -> std::false_type;
+
+        template <typename SS>
+        static auto test_end(int)
+            -> decltype ( std::declval<SS>().end() , std::true_type() );
+
+        template <typename>
+        static auto test_end(...) -> std::false_type;
+
+      public:
+
+        static const bool value = decltype(test_begin<S>(0))::value && decltype (test_end<S>(0))::value;
+
+    };
+
+    template <typename S>
+    class has_size
+    {
+        template<typename SS>
+        static auto test(int)
+            -> decltype ( std::declval<SS>().size() , std::true_type() );
+
+        template<typename>
+        static auto test(...) -> std::false_type;
+
+    public:
+
+        static const bool value = decltype ( test<S>(0) )::value;
+
+    };
+
+    template <typename S>
+    class has_push_back
+    {
+        template<typename SS>
+        static auto test(int)
+            -> decltype( std::declval<SS>().push_back( std::declval<typename SS::value_type>() ) , std::true_type{} );
+
+        template<typename>
+        static auto test(...) -> std::false_type;
+
+
+    public:
+
+        static const bool value = decltype( test<S>(0) )::value;
+
+    };
+
+
+    template <typename S>
+    class has_at
+    {
+        template<typename SS>
+        static auto test(int)
+            -> decltype( std::declval<SS>().at( 0 ) , std::true_type{} );
+
+        template<typename>
+        static auto test(...) -> std::false_type;
+
+    public:
+
+        static const bool value = decltype( test<S>(0) )::value;
+
+    };
+
+    template<typename Cont>
+    struct is_container
+    {
+        static const bool value = has_begin_and_end<Cont>::value && has_size<Cont>::value && has_push_back<Cont>::value && has_at<Cont>::value;
+    };
 
 }
 
