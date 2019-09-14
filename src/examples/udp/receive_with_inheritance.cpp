0455a66ca8d22923f23fe195ebb3a23862d012a6
ozanc <ozancansel@gmail.com>
2019-09-14 13:27:48 +0300

    format

--- src/examples/udp/receive_with_inheritance.cpp
+++ src/examples/udp/receive_with_inheritance.cpp
@@ -7,50 +7,45 @@
 
 struct my_receiver : speech::udp::udp_receiver<greeting, roll_dice> {
 
-     explicit my_receiver ( speech::port p )
+    explicit my_receiver ( speech::port p )
         : speech::udp::udp_receiver<greeting, roll_dice> { p }
     { }
 
 protected:
 
-     void on_receive ( const greeting& greeting ) override
-     {
-          qDebug() << "receive => " << greeting;
-     }
+    void on_receive ( const greeting& greeting ) override
+    {
+        qDebug() << "receive => " << greeting;
+    }
 
-     void on_receive ( const roll_dice& dice ) override
-     {
-          qDebug() << "receive => " << dice;
-     }
+    void on_receive ( const roll_dice& dice ) override
+    {
+        qDebug() << "receive => " << dice;
+    }
 
 };
 
 int main ( int argc, char** argv )
 {
-     QCoreApplication app ( argc, argv );
+    QCoreApplication app ( argc, argv );
 
-     QCommandLineParser parser;
-     parser.addHelpOption();
+    QCommandLineParser parser;
+    parser.addHelpOption();
 
-     parser.addOptions ( {{ {"p", "port"}, "Specify port number", "port number" }} );
+    parser.addOptions ( {{ {"p", "port"}, "Specify port number", "port number" }} );
 
-     parser.process ( app );
+    parser.process ( app );
 
-     //Defaults
-     auto port = 24942;
+    //Defaults
+    auto port = 24942;
 
-     if ( parser.isSet ( "p" ) ) {
-          port = parser.value ( "p" ).toInt();
-     }
+    if ( parser.isSet ( "p" ) ) {
+        port = parser.value ( "p" ).toInt();
+    }
 
+    my_receiver receiver{ speech::port{port} };
 
-    my_receiver receiver{ speech::port{ port } };
-    qDebug() << "Moving";
-    auto moved_received = std::move(receiver);
-    qDebug() << "Moved";
-//    my_receiver receiver{ speech::port{port} };
+    qDebug() << "Udp port " << port << " is listening.";
 
-     qDebug() << "Udp port " << port << " is listening.";
-
-     return app.exec();
+    return app.exec();
 }
