01b48a5923505a1354a834687460468135ddcbfd
ozanc <ozancansel@gmail.com>
2019-09-17 20:52:56 +0300

    for_each and forward_handler moved to impl namespace, declaration and definitions are splitted to .h and _impl.h

--- src/speech/tcp/tcp_server.h
+++ src/speech/tcp/tcp_server.h
@@ -4,7 +4,6 @@
 #include <QTcpServer>
 #include <QTcpSocket>
 #include <QHostAddress>
-#include <unordered_map>
 #include <tuple>
 #include <functional>
 #include <memory>
@@ -18,6 +17,9 @@ namespace speech
 namespace tcp
 {
 
+template<typename T>
+class handler;
+
 namespace impl
 {
 
@@ -28,6 +30,16 @@ struct lifetime
 
     cb_type cb;
 };
+
+template <typename Tuple, typename F, std::size_t ...Indices>
+void for_each_impl(Tuple&& tuple, F&& f, std::index_sequence<Indices...>);
+
+template <typename Tuple, typename F>
+void for_each(Tuple&& tuple, F&& f);
+
+template<typename... T , typename E , size_t... I>
+auto forward_handler( std::tuple<T...>&& partial_cont , handler<E>&& rhs , std::index_sequence<I...> );
+
 }
 
 template<typename T>
@@ -38,41 +50,16 @@ public:
 
     using client_cb = std::function<void( const T& , QTcpSocket& )>;
 
-    handler( client_cb&& cb , std::shared_ptr<impl::lifetime> c )
-        : m_conn { std::move( c ) }
-        , m_cb { std::forward<client_cb>( cb ) }
-    {
-        using namespace std::placeholders;
-       m_conn->cb = std::bind( &tcp_receiver<T>::on_data_received , this , _1 , _2 );
-    }
-
+    handler( client_cb&& cb , std::shared_ptr<impl::lifetime> c );
     ~handler() noexcept = default;
     handler( const handler& ) = delete;
     handler& operator = ( const handler& ) = delete;
-    handler( handler&& other ) noexcept
-    {
-        using namespace std::placeholders;
-
-        m_conn = other.m_conn;
-        m_cb = other.m_cb;
-        m_conn->cb = std::bind( &tcp_receiver<T>::on_data_received , this , _1 , _2 );
-    }
-
-    handler& operator = ( handler&& other ) noexcept
-    {
-        using namespace std::placeholders;
-
-        m_conn = other.m_conn;
-        m_cb = other.m_cb;
-        m_conn->cb = std::bind( tcp_receiver<T>::on_data_received , this , _1 , _2 );
-    }
+    handler( handler&& other ) noexcept;
+    handler& operator = ( handler&& other ) noexcept;
 
 protected:
 
-    void on_receive( const T& e ) override
-    {
-        m_cb( e , tcp_receiver<T>::socket() );
-    }
+    void on_receive( const T& e ) override;
 
 private:
 
@@ -83,56 +70,24 @@ private:
 
 };
 
-template <typename Tuple, typename F, std::size_t ...Indices>
-void for_each_impl(Tuple&& tuple, F&& f, std::index_sequence<Indices...>) {
-    using swallow = int[];
-    (void)swallow{1,
-        (f(std::get<Indices>(std::forward<Tuple>(tuple))), void(), int{})...
-    };
-}
-
-template <typename Tuple, typename F>
-void for_each(Tuple&& tuple, F&& f) {
-    constexpr std::size_t N = std::tuple_size<std::remove_reference_t<Tuple>>::value;
-    for_each_impl(std::forward<Tuple>(tuple), std::forward<F>(f),
-                  std::make_index_sequence<N>{});
-}
-
 class tcp_server
 {
 public:
 
-    tcp_server(const QHostAddress &address = QHostAddress::Any, speech::port = speech::port(0));
-    int port() const { return m_server.serverPort(); }
+    tcp_server( const QHostAddress &address = QHostAddress::Any , speech::port = speech::port(0) );
+    int port() const;
 
     template<typename... T>
-    tcp_server& operator | ( std::tuple<handler<T>...>& handlers )
-    {
-        for_each( handlers , [ this ]( auto& handler ) { m_lifetimes.push_back( handler.m_conn ); } );
-
-        return *this;
-    }
-
-    template<typename... T>
-    tcp_server& listen( std::tuple<handler<T>...>& handlers )
-    {
-        for_each( handlers , [ this ]( auto& handler ) { m_lifetimes.push_back( handler.m_conn ); } );
-
-        return *this;
-    }
+    tcp_server& listen( const std::tuple<handler<T>...>& handlers );
 
     template<typename T>
-    tcp_server& listen( handler<T>& handler )
-    {
-        m_lifetimes.push_back( handler.m_conn );
-        return *this;
-    }
+    tcp_server& listen( const handler<T>& handler );
 
 private:
 
     int ready_read_callback( const QByteArray& , QTcpSocket& );
     void new_connection();
-    void disconnected ( QTcpSocket* s );
+    void disconnected ( QTcpSocket* );
     std::vector<shared_socket<QTcpSocket>>  m_alive_connections;
     std::vector<std::shared_ptr<impl::lifetime>> m_lifetimes;
     QTcpServer m_server;
@@ -140,31 +95,13 @@ private:
 };
 
 template<typename T>
-handler<T> listen( typename handler<T>::client_cb&& cb )
-{
-    return handler<T> {  std::forward<typename handler<T>::client_cb>( cb ) , std::make_shared<impl::lifetime>() };
-}
+handler<T> listen( typename handler<T>::client_cb&& cb );
 
 template< typename L , typename R >
-std::tuple< handler<L> , handler<R> > operator | ( handler<L>&& lhs , handler<R>&& rhs )
-{
-    return std::make_tuple( std::forward<handler<L>>( lhs ) , std::forward<handler<R>>( rhs ) );
-}
-
-template<typename... T , typename C , size_t... I>
-auto forward_handler( std::tuple<T...>&& partial_cont , handler<C>&& rhs , std::index_sequence<I...> )
-{
-    using namespace std;
-
-    return tuple<T... , handler<C>>( move( get<I>( partial_cont ) )... , forward<handler<C>>( rhs ));
-}
+std::tuple< handler<L> , handler<R> > operator | ( handler<L>&& lhs , handler<R>&& rhs );
 
 template< typename... L , typename R >
-auto operator | ( std::tuple<L...>&& lhs , handler<R>&& rhs )
-{
-    using namespace std;
-    return forward_handler( forward<tuple<L...>>( lhs ) ,  forward<handler<R>>( rhs ), std::index_sequence_for< L... >() );
-}
+auto operator | ( std::tuple<L...>&& lhs , handler<R>&& rhs );
 
 }
 }
--- src/speech/tcp/tcp_server_impl.h
+++ src/speech/tcp/tcp_server_impl.h
@@ -10,6 +10,69 @@ namespace speech
 namespace tcp
 {
 
+namespace impl
+{
+
+    template <typename Tuple, typename F, std::size_t ...Indices>
+    void for_each_impl(Tuple&& tuple, F&& f, std::index_sequence<Indices...>) {
+        using swallow = int[];
+        (void)swallow{1,
+            (f(std::get<Indices>(std::forward<Tuple>(tuple))), void(), int{})...
+        };
+    }
+
+    template <typename Tuple, typename F>
+    void for_each(Tuple&& tuple, F&& f) {
+        constexpr std::size_t N = std::tuple_size<std::remove_reference_t<Tuple>>::value;
+        for_each_impl(std::forward<Tuple>(tuple), std::forward<F>(f),
+                      std::make_index_sequence<N>{});
+    }
+
+    template<typename... T , typename E , size_t... I>
+    auto forward_handler( std::tuple<T...>&& partial_cont , handler<E>&& rhs , std::index_sequence<I...> )
+    {
+        using namespace std;
+
+        return tuple<T... , handler<E>>( move( get<I>( partial_cont ) )... , forward<handler<E>>( rhs ));
+    }
+
+}
+
+template<typename T>
+handler<T>::handler( client_cb&& cb , std::shared_ptr<impl::lifetime> c )
+    : m_conn { std::move( c ) }
+    , m_cb { std::forward<client_cb>( cb ) }
+{
+    using namespace std::placeholders;
+    m_conn->cb = std::bind( &tcp_receiver<T>::on_data_received , this , _1 , _2 );
+}
+
+template<typename T>
+handler<T>::handler( handler<T>&& other ) noexcept
+{
+    using namespace std::placeholders;
+
+    m_conn = other.m_conn;
+    m_cb = other.m_cb;
+    m_conn->cb = std::bind( &tcp_receiver<T>::on_data_received , this , _1 , _2 );
+}
+
+template<typename T>
+handler<T>& handler<T>::operator = ( handler&& other ) noexcept
+{
+    using namespace std::placeholders;
+
+    m_conn = other.m_conn;
+    m_cb = other.m_cb;
+    m_conn->cb = std::bind( tcp_receiver<T>::on_data_received , this , _1 , _2 );
+}
+
+template <typename T>
+void handler<T>::on_receive( const T& e )
+{
+    m_cb( e , tcp_receiver<T>::socket() );
+}
+
 tcp_server::tcp_server ( const QHostAddress &address, speech::port p )
 {
     using namespace speech::error;
@@ -91,5 +154,45 @@ int tcp_server::ready_read_callback( const QByteArray& data , QTcpSocket& sck )
 
     return max_res;
 }
+
+int tcp_server::port() const
+{
+    return m_server.serverPort();
+}
+
+template<typename... T>
+tcp_server& tcp_server::listen( const std::tuple<handler<T>...>& handlers )
+{
+    impl::for_each( handlers , [ this ]( auto& handler ) { m_lifetimes.push_back( handler.m_conn ); } );
+
+    return *this;
+}
+
+template<typename T>
+tcp_server& tcp_server::listen( const handler<T>& handler )
+{
+    m_lifetimes.push_back( handler.m_conn );
+    return *this;
+}
+
+template<typename T>
+handler<T> listen( typename handler<T>::client_cb&& cb )
+{
+    return handler<T> {  std::forward<typename handler<T>::client_cb>( cb ) , std::make_shared<impl::lifetime>() };
+}
+
+template< typename L , typename R >
+std::tuple< handler<L> , handler<R> > operator | ( handler<L>&& lhs , handler<R>&& rhs )
+{
+    return std::make_tuple( std::forward<handler<L>>( lhs ) , std::forward<handler<R>>( rhs ) );
+}
+
+template< typename... L , typename R >
+auto operator | ( std::tuple<L...>&& lhs , handler<R>&& rhs )
+{
+    using namespace std;
+    return impl::forward_handler( forward<tuple<L...>>( lhs ) ,  forward<handler<R>>( rhs ), std::index_sequence_for< L... >() );
+}
+
 }
 }
